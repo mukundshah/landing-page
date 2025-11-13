f59188b5a46ae697ffe2816c6dd7b538ab07147f
Mukund Shah <39938037+mukundshah@users.noreply.github.com>
2025-11-14 00:21:04 +0545

    feat: integrate Header and Footer into app.vue

---
+++ app/app.vue
@@ -0,0 +1,12 @@
+<template>
+  <Html :class="`${$colorMode.preference}`">
+    <Body class="flex items-center justify-center bg-background">
+      <NuxtRouteAnnouncer />
+      <main class="flex min-h-screen w-screen max-w-5xl flex-col items-center px-6 pb-10 pt-7 font-satoshi text-[0.92rem] leading-relaxed sm:px-10 lg:px-10">
+        <SiteHeader />
+        <NuxtPage />
+        <SiteFooter />
+      </main>
+    </Body>
+  </Html>
+</template>
