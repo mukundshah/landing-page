f69dd15b5040af1d158bda1bbfe5c9960eb0cff4
Mukund Shah <39938037+mukundshah@users.noreply.github.com>
2025-11-14 00:20:10 +0545

    feat: add Header and Footer components

---
+++ app/components/site/Header.vue
@@ -0,0 +1,77 @@
+<script setup lang="ts">
+const colorMode = useColorMode()
+
+const toggleTheme = () => {
+  if (colorMode.preference === 'dark') {
+    colorMode.preference = 'light'
+  } else {
+    colorMode.preference = 'dark'
+  }
+}
+</script>
+
+<template>
+  <header class="mb-12 flex w-full flex-wrap pb-3 text-sm sm:flex-nowrap sm:justify-start">
+    <nav
+      aria-label="global"
+      class="relative mx-auto flex w-full items-center justify-between sm:flex sm:items-center"
+    >
+      <NuxtLink
+        aria-label="Brand"
+        class="flex-none text-xl font-semibold"
+        to="/"
+      >
+        wricha
+      </NuxtLink>
+
+      <div class="flex flex-row items-center justify-center gap-x-5 sm:gap-x-7">
+        <NuxtLink
+          aria-label="Nav Menu Item"
+          class="flex-none text-[1.05rem] font-medium hover:text-foreground/75"
+          to="/blogs"
+        >
+          Blog
+        </NuxtLink>
+        <NuxtLink
+          aria-label="Nav Menu Item"
+          class="flex-none text-[1.05rem] font-medium hover:text-foreground/75"
+          to="/tools"
+        >
+          Tools
+        </NuxtLink>
+
+        <button
+          class="relative rounded-md border border-border p-1.5 transition-all hover:bg-border"
+          type="button"
+          @click="toggleTheme"
+        >
+          <span class="sr-only">{{ $colorMode.preference === 'dark' ? 'Light Theme' : 'Dark Theme' }}</span>
+          <svg
+            class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:hidden dark:-rotate-90 dark:scale-0"
+            height="32"
+            viewBox="0 0 24 24"
+            width="32"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M12 15q1.25 0 2.125-.875T15 12q0-1.25-.875-2.125T12 9q-1.25 0-2.125.875T9 12q0 1.25.875 2.125T12 15m0 1q-1.671 0-2.836-1.164T8 12q0-1.671 1.164-2.836T12 8q1.671 0 2.836 1.164T16 12q0 1.671-1.164 2.836T12 16m-7-3.5H1.5v-1H5zm17.5 0H19v-1h3.5zM11.5 5V1.5h1V5zm0 17.5V19h1v3.5zM6.746 7.404l-2.16-2.098l.695-.744l2.111 2.134zM18.72 19.438l-2.117-2.14l.652-.702l2.16 2.098zM16.596 6.746l2.098-2.16l.744.695l-2.134 2.111zM4.562 18.72l2.14-2.117l.663.652l-2.078 2.179zM12 12"
+              fill="currentColor"
+            />
+          </svg>
+          <svg
+            class="hidden h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:block dark:rotate-0 dark:scale-100"
+            height="32"
+            viewBox="0 0 24 24"
+            width="32"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M12.058 20q-3.334 0-5.667-2.333Q4.058 15.333 4.058 12q0-3.038 1.98-5.27Q8.02 4.5 10.942 4.097q.081 0 .159.006t.153.017q-.506.706-.801 1.57q-.295.865-.295 1.811q0 2.667 1.866 4.533q1.867 1.867 4.534 1.867q.952 0 1.813-.295q.862-.295 1.548-.801q.012.075.018.153q.005.078.005.158q-.384 2.923-2.615 4.904T12.057 20"
+              fill="currentColor"
+            />
+          </svg>
+        </button>
+      </div>
+    </nav>
+  </header>
+</template>
